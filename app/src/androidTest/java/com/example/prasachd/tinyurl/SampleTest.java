fd24c6354ea54271a9af51a8eb6055db4fc4ff77
Prateek Sachdeva <sachdeva852000@gmail.com>
2018-06-13 15:44:33 -0700

    Sample test file

---
+++ app/src/androidTest/java/com/example/prasachd/tinyurl/SampleTest.java
@@ -0,0 +1,12 @@
+package com.example.prasachd.tinyurl;
+
+import org.junit.Test;
+import junit.framework.Assert;
+
+public class SampleTest {
+
+    @Test
+    public void testGetSortedConversations() {
+        Assert.assertEquals(true, true);
+    }
+}
